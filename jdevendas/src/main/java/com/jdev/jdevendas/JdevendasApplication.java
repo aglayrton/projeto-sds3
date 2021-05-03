9e47cc64b64cd3a47151d9950f6a4ec12b6ace71
Aglayrton Julião <aglayrtonjuliao@gmail.com>
2021-05-03 14:22:47 -0300

    Project created

---
+++ jdevendas/src/main/java/com/jdev/jdevendas/JdevendasApplication.java
@@ -0,0 +1,13 @@
+package com.jdev.jdevendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdevendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdevendasApplication.class, args);
+	}
+
+}
---
+++ jdevendas/src/test/java/com/jdev/jdevendas/JdevendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jdev.jdevendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdevendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
