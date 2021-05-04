feabfcd70ce4066f3775706065b91fe1bba34389
Aglayrton Julião <aglayrtonjuliao@gmail.com>
2021-05-04 18:12:02 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,24 @@
 import Navbar from 'components/Navbar';
 import Footer from 'components/Footer';
 import Datatable from 'components/Datatables';
+import BarChart from 'components/BarChart';
+import DonutChart from 'components/DonutChart';
 
 function App() {
   return (
     <>
       <Navbar />
-      <Datatable />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6">
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <DonutChart />
+          </div>
+        </div>
+        <Datatable />
+      </div>
       <Footer />
     </>
   );
--- frontend/src/components/Datatables/index.tsx
+++ frontend/src/components/Datatables/index.tsx
@@ -19,6 +19,35 @@ const Datatable = () => {
                         <td>25</td>
                         <td>15017.00</td>
                     </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    <tr>
+                        <td>22/04/2021</td>
+                        <td>Barry Allen</td>
+                        <td>34</td>
+                        <td>25</td>
+                        <td>15017.00</td>
+                    </tr>
+                    
                 </tbody>
             </table>
         </div>
